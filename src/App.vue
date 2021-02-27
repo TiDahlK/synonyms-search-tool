6fe869ac7c69280daaa2ef7f00023388334b1fa5
Tim Dahl K <ent@live.se>
2021-02-27 10:12:02 +0100

    added autocomplete and started on search logic

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Synonyms search tool</h3>
+    <autocomplete
+      class="autocomplete"
+      :search="search"
+      auto-select
+      @submit="onSubmit"
+    ></autocomplete>
+    <ul id="selectedGraph">
+      <li v-for="word in selectedGraph" :key="word">
+        {{ word }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Autocomplete from "@trevoreyre/autocomplete-vue";
+import "@trevoreyre/autocomplete-vue/dist/style.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Autocomplete,
+  },
+  data() {
+    return {
+      dictionary: [
+        {
+          value: "wash",
+          index: "0",
+          graphIndex: "0",
+        },
+        {
+          value: "clean",
+          index: 1,
+          graphIndex: 0,
+        },
+        {
+          value: "dirty",
+          index: 0,
+          graphIndex: 1,
+        },
+        {
+          value: "unclean",
+          index: 1,
+          graphIndex: 1,
+        },
+      ],
+      graphs: [
+        ["wash", "clean"],
+        ["dirty", "unclean"],
+      ],
+      selectedGraph: [],
+    };
+  },
+  methods: {
+    search(input) {
+      if (input.length < 1) {
+        return [];
+      }
+      const foundWords = this.findWords(input);
+      const values = foundWords.map(({ value }) => value);
+      return values;
+    },
+    findWords(input) {
+      return this.dictionary.filter((word) => {
+        return word.value.toLowerCase().startsWith(input.toLowerCase());
+      });
+    },
+    onSubmit(result) {
+      if (!result) {
+        return;
+      }
+      const foundWord = this.findWords(result)[0];
+      this.selectedGraph = this.graphs[foundWord.graphIndex];
+    },
+  },
+};
 </script>
 
 <style>
